c12ab9883c84a737943b5fee6c72a769a2a67838
Spack <hunterspack@gmail.com>
2020-10-10 14:31:25 -0400

    added color.sh

---
+++ color.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+default='\033[0m'
+
+read -p "What is your favorite color (red, green, yellow, blue, magenta)? " color
+
+color=${color^^}
+
+case "$color" in
+RED)	echo "${color} was chosen"
+	selected_color=$red
+	echo -e "${selected_color} Your favorite color is ${color}${default}"
+
+	;;
+
+
+GREEN)	echo "${color} was chosen"
+	selected_color=$green
+	echo -e "${selected_color} Your favorite color is ${color}${default}"
+
+	;;
+
+
+YELLOW)	echo "${color} was chosen"
+	selected_color=$yellow
+	echo -e "${selected_color} Your favorite color is ${color}${default}"
+
+	;;
+
+
+BLUE)	echo "${color} was chosen"
+	selected_color=$blue
+	echo -e "${selected_color} Your favorite color is ${color}${default}"
+
+	;;
+
+
+MAGENTA)	echo "${color} was chosen"
+	selected_color=$magenta
+	echo -e "${selected_color} Your favorite color is ${color}${default}"
+
+	;;
+
+
+*)	echo "Please enter a valid color" >&2
+	exit 1
+	;;
+esac
